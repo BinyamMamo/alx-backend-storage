eddd6bb24c98eaac34c376965e97d45a02fdc39c
BinyamMamo <binyammamo01@gmail.com>
2024-03-21 22:50:45 +0400

    🔄️ refactor code

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -4,33 +4,10 @@ DELIMITER //
 
 CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
 BEGIN
-    DECLARE total_score DECIMAL(10,2);
-    DECLARE total_projects INT;
-    DECLARE average DECIMAL(10,2);
-
-    -- Calculate total score for the user
-    SELECT SUM(score)
-    INTO total_score
-    FROM corrections
-    WHERE user_id = user_id;
-
-    -- Calculate total number of projects for the user
-    SELECT COUNT(*)
-    INTO total_projects
-    FROM corrections
-    WHERE user_id = user_id;
-
-    -- Calculate average score
-    IF total_projects > 0 THEN
-        SET average = total_score / total_projects;
-    ELSE
-        SET average = 0;
-    END IF;
-
-    -- Update average score for the user
-    UPDATE users
-    SET average_score = average
-    WHERE id = user_id;
+	UPDATE users
+	SET
+	average_score = (SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id)
+	WHERE id = user_id;
 END //
 
 DELIMITER ;
