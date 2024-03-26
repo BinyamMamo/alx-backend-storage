525fe572cd95ecc993ff3605b344589c5709340e
BinyamMamo <binyammamo01@gmail.com>
2024-03-27 00:06:45 +0400

    chore: add task 10

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Task 10 - Change school topics
+Write a Python function that changes all
+topics of a school document based on the name
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+     Update topics for a user
+    """
+    try:
+        query = {"name": name}
+        doc = mongo_collection.find_one(query)
+
+        if doc:
+            update = {"$set": {"topics": topics}}
+            mongo_collection.update_one(query, update)
+            return str(doc["_id"])
+        else:
+            return None
+    except Exception as e:
+        return None
