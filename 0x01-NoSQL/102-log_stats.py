bddf17679c249010ccae2624ee2e687cc5591e13
BinyamMamo <binyammamo01@gmail.com>
2024-03-27 01:18:34 +0400

    refactor code

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -4,43 +4,50 @@ Task 15 - Log stats - new version (#advanced)
 Improve 12-log_stats.py by adding the top 10 of the most
 present IPs in the collection nginx of the database logs
 """
+from pymongo import MongoClient
 
 
-def top_ips(mongo_collection):
+def print_nginx_request_logs(nginx_collection):
     """
-    Returns the top 10 most present IPs in the MongoDB collection.
+     Prints logs for requests.
     """
-    try:
-        pipeline = [
+    print('{} logs'.format(nginx_collection.count_documents({})))
+    print('Methods:')
+    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    for method in methods:
+        req_count = len(list(nginx_collection.find({'method': method})))
+        print('\tmethod {}: {}'.format(method, req_count))
+    status_checks_count = len(list(
+        nginx_collection.find({'method': 'GET', 'path': '/status'})
+    ))
+    print('{} status check'.format(status_checks_count))
+
+
+def print_top_ips(server_collection):
+    """
+     Print top 10 IP addresses
+    """
+    print('IPs:')
+    request_logs = server_collection.aggregate(
+        [
             {
-                "$group": {
-                    "_id": "$ip",
-                    "count": {"$sum": 1}
-                }
+                '$group': {'_id': "$ip", 'totalRequests': {'$sum': 1}}
             },
             {
-                "$sort": {
-                    "count": -1
-                }
+                '$sort': {'totalRequests': -1}
             },
             {
-                "$limit": 10
-            }
+                '$limit': 10
+            },
         ]
+    )
+    for request_log in request_logs:
+        ip = request_log['_id']
+        ip_requests_count = request_log['totalRequests']
+        print('\t{}: {}'.format(ip, ip_requests_count))
 
-        top_ips = list(mongo_collection.aggregate(pipeline))
-        return top_ips
-    except Exception as e:
-        return []
 
-
-if __name__ == "__main__":
-    from pymongo import MongoClient
-
-    client = MongoClient("mongodb://localhost:27017/")
-    db = client["logs"]
-    collection = db["nginx"]
-
-    top_ips_list = top_ips(collection)
-    for ip in top_ips_list:
-        print(f"{ip['_id']}: {ip['count']}")
+if __name__ == '__main__':
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    print_nginx_request_logs(client.logs.nginx)
+    print_top_ips(client.logs.nginx)
