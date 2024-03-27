eb2dd5faaa7a8408615e7d1ab7366b2832af5876
BinyamMamo <binyammamo01@gmail.com>
2024-03-27 20:09:38 +0400

    fix: pass True to flushdb

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -19,7 +19,7 @@ class Cache():
          Initialize the Redis instance. This is called by __init__
         """
         self._redis = Redis()
-        self._redis.flushdb()
+        self._redis.flushdb(True)
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
